fc56c93096400644125f49fe53fa4a630c90b478
smalltoebigtoe <jn824824@gmail.com>
2022-11-26 20:13:58 -0500

    memory is cool

--- memory.cpp
+++ memory.cpp
@@ -1,12 +1,29 @@
 #include "memory.h"
 
+uint16_t Memory::get_page_offset(uint16_t page) {
+  uint16_t address = page * 0xFF;
+  if (page > 0)
+    address++;
+  return address;
+}
+
+Byte Memory::get_at(uint16_t address) {
+  return data[address];
+}
+void Memory::set_at(uint16_t address, Byte input) {
+  data[address] = input;
+}
+
+Byte Memory::get_at_page(uint16_t page, Byte offset) {
+  return get_at(get_page_offset(page) + offset);
+}
+
+void Memory::set_at_page(uint16_t page, Byte offset, Byte input) {
+  set_at(get_page_offset(page) + offset, input);
+}
+
 void Memory::reset() {
   for (u32 i = 0; i < MAX; i++) {
     data[i] = 0x0;
   }
-}
-
-Byte Memory::operator[](u32 address) const {
-  // assert(address > MAX);
-  return data[address];
 }
--- memory.h
+++ memory.h
@@ -2,14 +2,31 @@
 #define MEMORY_H
 
 #include "data.h"
+#include <stdexcept>
+
+/*
+There is 64KB of memory and this is broken into
+
+0x0000 - 0x00FF - Free (Zero Page)
+0x0100 - 0x01FF - Stack
+0x0200 - 0x05FF - Free
+0x0600 - 0xDFFF - Program - always make sure to assemble programs for this address
+0xF002 - 0xFFFF - Free        */
 
 struct Memory
 {
-  static const u32 MAX = 1024 * 64;
+  static const int MAX = 65535;
   Byte data[MAX];
 
+  uint16_t get_page_offset(uint16_t page);
+
+  void set_at(uint16_t address, Byte input);
+  Byte get_at(uint16_t address);
+
+  void set_at_page(uint16_t page, Byte offset, Byte input);
+  Byte get_at_page(uint16_t page, Byte offset);
+
   void reset();
-  Byte operator[](u32 address) const;
 };
 
 #endif
